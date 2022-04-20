ea22dc4febc33ef7e7e2d777130faebc671e6a4e
Emir <77904942+ReFo0@users.noreply.github.com>
2022-04-20 07:38:33 +0300

    Add files via upload

---
+++ hwid_info/main.cpp
@@ -0,0 +1,21 @@
+#include <Windows.h>
+#include <iostream>
+#include <consoleapi2.h>
+
+auto serials() -> bool
+{
+	system("\n\nwmic diskdrive get serialnumber");
+	system("\nwmic bios get serialnumber");
+	system("\nwmic cpu get serialnumber");
+	system("\nwmic baseboard get serialnumber");
+	system("\nwmic memorychip get serialnumber");
+	system("\nwmic desktopmonitor get Caption, MonitorType, MonitorManufacturer, Name");
+	system("\ngetmac");
+	std::cin.get();
+	return 0;
+
+}
+
+auto main() -> int {
+	serials();
+}
